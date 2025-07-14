b2101ecd586cdbaae1c07f100c53a55523d2d906
Otabek <otashqashqar@gmail.com>
2025-07-14 11:30:37 +0500

    animate progress bar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ProgressBar.css';
 
-const ProgressBar = ({progressValue}) => {
+const ProgressBar = ({ progressValue }) => {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setWidth(Math.max(0, Math.min(100, progressValue)));
+    }, 100);
+
+    return () => clearTimeout(timer);
+  }, [progressValue]);
+
+  const getBackgroundColor = (value) => {
+    if (value < 33) return 'progress-red';
+    if (value < 66) return 'progress-yellow';
+    if (value < 100) return 'progress-green';
+    return 'progress-gold';
+  };
+
   return (
-    <div className='relative'>
+    <div className="relative">
       <div className="progress-bar">
-        <div className="progress-bar-inner" style={{width: `${progressValue}%`}}></div>
-        <div className="my-progress-value">
-          {progressValue}%
-        </div>
+        <div
+          className={`progress-bar-inner ${getBackgroundColor(width)}`}
+          style={{ width: `${width}%` }}
+        ></div>
+        <div className="my-progress-value">{progressValue}%</div>
       </div>
     </div>
   );

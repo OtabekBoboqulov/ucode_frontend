dba91e4128c9c3ef5bd8891ed6f8e203e8acdc80
Otabek <otashqashqar@gmail.com>
2025-07-18 16:33:40 +0500

    change BASE_URL constant

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://web-production-d4808.up.railway.app";
 export const MEDIA_BASE_URL = "https://res.cloudinary.com/bnf404/";

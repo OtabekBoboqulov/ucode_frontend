d3f6a46bc2c8b163302a7ff69f9ce32101e686f7
Otabek <otashqashqar@gmail.com>
2025-07-18 17:35:56 +0500

    fix header and intro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -19,15 +19,13 @@ const Intro = () => {
       <div className="intro-text">
         Mamatqulov Ulug`bek Babakulovich. <br/>
         1979 yil 7 sentyabrda O`zbekiston Respublikasi, Qashqadaryo viloyat Koson tumanida, ziyoli oilasida tug`ilgan.
-        1996 yili Koson tumanidagi 9-sonli Hamid Olimjon nomli maktabni oltin medal bilan tamomlab, shu yili Qashqadaryo
-        viloyat Qarshi Davlat universitetiga, "Amaliy matematika va informatika" fakultetiga, byudjet asosida o`qishga
-        qabul qilingan. <br/>
-        2000 yili o`qishni tamomlab, Koson tumanidagi 1-2-9-92-maktablarda informatika va AT fani o`qituvchisi
-        lavosimida faoliyat yuritgan. <br/>
+        1996 yili Koson tumanidagi 9-sonli maktabni oltin medal bilan tamomlab, Qashqadaryo viloyat Qarshi Davlat
+        universitetiga, "Amaliy matematika va informatika" fakultetiga, byudjet asosida o`qishga qabul qilingan.
+        2000 yildan Koson tumanidagi 1-2-9-92-maktablarda informatika va AT fani o`qituvchisi lavosimida faoliyat
+        yuritgan. <br/>
         2002-2017 yillar davomida Koson kompyuter texnologiyalari kasb-hunar kollejida Axborot texnologiyalari maxsus
         fanlari o`qituvchisi, bo`lim boshlig`i, bosh usta, uslubchi va kollej direktori lavozimlarida faoliyat yuritgan.
-        Hozirgi kunda Qashqadaryo viloyat Koson tuman 9-maktabda Informatika va AT fani o`qituvchisi lavozimida faoliyat
-        yuritadi. <br/>
+        Hozirda Koson tuman 9-maktabda Informatika va AT fani o`qituvchisi lavozimida faoliyat yuritadi.
         Oliy toifali, Xalq ta`limi a`lochisi, "Bir million o`zbek dasturchilari" loyihasi Respublika bosqichi g`olibi,
         ishlari Respublika miqyosida ommalashtirilgan.
       </div>

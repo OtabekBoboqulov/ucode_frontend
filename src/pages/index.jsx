36a4819226a84462a79953338c1796b20a363f45
Otabek <otashqashqar@gmail.com>
2025-07-27 09:40:20 +0500

    add course search in main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -22,6 +22,18 @@ const Index = () => {
   const navigate = useNavigate();
   const [isDeleteAskOpen, setIsDeleteAskOpen] = useState(false)
   const [deleteCourseId, setDeleteCourseId] = useState(null);
+  const [searchText, setSearchText] = useState('');
+  const [searchFilter, setSearchFilter] = useState('');
+
+  const handleSearchTextChange = (e) => {
+    const text = e.target.value;
+    setSearchText(text);
+  };
+
+  const handleSearchFilterChange = (e) => {
+    const filter = e.target.value;
+    setSearchFilter(filter);
+  }
 
   const closeDeleteAskModal = () => {
     setIsDeleteAskOpen(false);
@@ -133,24 +145,50 @@ const Index = () => {
         <div className="container mx-auto pt-10">
           <Intro/>
           <Section title="Kurslar" textSize='text-4xl'/>
+          <div className="my-courses-toolbar">
+            <input type="text" placeholder="Kurslarni qidirish..." className="course-searchbar"
+                   onChange={handleSearchTextChange} value={searchText}/>
+            <div className="relative inline-block">
+              <select className="search-filter" onChange={handleSearchFilterChange} defaultValue={searchFilter}>
+                <option value="">Hammasi</option>
+                <option value="junior">Junior</option>
+                <option value="middle">Middle</option>
+                <option value="senior">Senior</option>
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-3 flex items-center">
+                <svg
+                  className="h-4 w-4 text-gray-500"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"/>
+                </svg>
+              </div>
+            </div>
+          </div>
           <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-12" id="courses">
             {courses.length > 0 ? (
-              courses.map((course) => (
-                <div className="relative">
-                  <CourseCard course={course}
-                              btn_text={enrolledCourses.includes(course.id) ? 'Davom etish' : 'Boshlash'}
-                              key={course.id}/>
-                  {isAuthorized() && isStaff() && (
-                    <button className="delete-course-btn" onClick={() => openDeleteAskModal(course.id)}>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                           stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"/>
-                      </svg>
-                    </button>
-                  )}
-                </div>
-              ))
+              courses.filter((course) => {
+                 return course.name.toLowerCase().includes(searchText.toLowerCase()) &&
+                course.complexity.toLowerCase().includes(searchFilter.toLowerCase())
+              }).map((course) => {
+                return (
+                  <div className="relative">
+                      <CourseCard course={course}
+                                  btn_text={enrolledCourses.includes(course.id) ? 'Davom etish' : 'Boshlash'}
+                                  key={course.id}/>
+                      {isAuthorized() && isStaff() && (
+                        <button className="delete-course-btn" onClick={() => openDeleteAskModal(course.id)}>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                               stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round"
+                                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"/>
+                          </svg>
+                        </button>
+                      )}
+                  </div>
+                )
+              })
             ) : ''}
             {isAuthorized() && JSON.parse(localStorage.getItem('loginData'))?.is_staff && <CreateCourse/>}
           </section>
